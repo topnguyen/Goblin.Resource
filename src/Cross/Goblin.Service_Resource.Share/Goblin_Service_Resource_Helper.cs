0ac54a0522b53d5a40b1735150887ac2d0fa9645
Top Nguyen <admin@topnguyen.com>
2020-06-23 23:33:40 +0700

    improve Error Model and Create Lib

---
+++ src/Cross/Goblin.Service_Resource.Share/Goblin_Service_Resource_Helper.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+using Goblin.Core.Errors;
+using Goblin.Service_Resource.Share.Models;
+
+namespace Goblin.Service_Resource.Share
+{
+    public static class Goblin_Service_Resource_Helper
+    {
+        public static string Domain { get; set; } = string.Empty;
+        
+        public static async Task<FileModel> UploadAsync(UploadFileModel model, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var fileModel = await Domain
+                    .AppendPathSegment(Endpoints.UploadFile)
+                    .PostJsonAsync(model, cancellationToken: cancellationToken)
+                    .ReceiveJson<FileModel>();
+
+                return fileModel;
+            }
+            catch (FlurlHttpException ex)
+            {
+                
+                var goblinErrorModel = await ex.GetResponseJsonAsync<GoblinErrorModel>().ConfigureAwait(true);
+
+                if (goblinErrorModel != null)
+                {
+                    throw new GoblinException(goblinErrorModel);
+                }
+                
+                var responseString = await ex.GetResponseStringAsync().ConfigureAwait(true);
+                    
+                var message = responseString ?? ex.Message;
+                
+                throw new Exception(message);
+
+            }
+        }
+    }
+}
