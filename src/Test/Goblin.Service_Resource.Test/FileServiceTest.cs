e3924fe9afe21cc41a8064cef7effdf618128c57
Top Nguyen <admin@topnguyen.com>
2020-06-24 12:01:10 +0700

    add test project

---
+++ src/Test/Goblin.Service_Resource.Test/FileServiceTest.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+
+namespace Goblin.Service_Resource.Test
+{
+    public class FileServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void UploadTest()
+        {
+            Assert.Pass();
+        }
+        
+        [Test]
+        public void GetTest()
+        {
+            Assert.Pass();
+        }
+        
+        [Test]
+        public void DeleteTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
