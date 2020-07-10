0a1a9b0bd7a156343bb10e157e1b29f2c0b714f4
Top Nguyen <admin@topnguyen.com>
2020-07-10 17:19:51 +0700

    update lib

--- src/Cross/Goblin.Resource.Share/FlurlHttpExceptionHelper.cs
+++ src/Cross/Goblin.Resource.Share/FlurlHttpExceptionHelper.cs
@@ -18,7 +18,7 @@ namespace Goblin.Resource.Share
 
             var responseString = await ex.GetResponseStringAsync().ConfigureAwait(true);
 
-            var message = responseString ?? ex.Message;
+            var message = string.IsNullOrWhiteSpace(responseString) ? ex.Message : responseString;
 
             throw new Exception(message);
         }
